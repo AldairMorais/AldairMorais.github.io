6ffe60a75cd523edad7ba223e58c5670a26cf118
Aldair <aldaiirmoraiss@gmail.com>
2022-07-10 09:24:52 +0100

    last

---
+++ Code/js/script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById('toggle');
+const menuUp = document.getElementById('menuId');
+const rmvMn = document.getElementById('removeMn');
+toggle.onclick = function () {
+    menuUp.classList.toggle('active');
+    // menuUp.style.position = 'absolute';
+    // menuUp.classList.add('fadeInLeft');
+}
+
+rmvMn.onclick = function () {
+    menuUp.style.display = 'none';
+}
+
+
+
